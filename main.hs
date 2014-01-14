737249685cc5f1c08fb1e7a089c6ddc3e8e1386b
Jean-Alexandre Peyroux <m@j4.pe>
2014-01-14 20:16:40 +0100

    ajout de la class Sync et data SyncFile et SyncAction

--- main.hs
+++ main.hs
@@ -1,37 +1,42 @@
-import System.INotify
+import qualified System.INotify as IN 
+
+data SyncAction = Create | Update | Delete | Move deriving Show
 
 data SyncFile = SyncFile {
-  sfPath :: FilePath
-  } deriving Show
+  sfPath :: FilePath,
+  sfAction :: SyncAction
+} deriving Show
 
 class Sync s where
-  sync :: s -> IO() -- mettre un functor or monade ?
+  sync :: s -> IO() -- mettre un functor or monade pour generic sync (ftp, rsync, ws ...) ?
   delete :: s -> String
   move :: s -> String
   create :: s -> String
+  modif :: s -> String
 
 instance Sync SyncFile where
-  sync s = print "sync action baby" 
-  delete s = "del " ++ sfPath s
-  move s = "move " ++ sfPath s
-  create s = "create " ++ sfPath s
+  sync (SyncFile p a) = print "sync action baby" 
+  delete (SyncFile p a) = "(" ++ show (sfAction (SyncFile p a)) ++ ") - " ++ (sfPath (SyncFile p a))
+  move (SyncFile p a) = "(" ++ show (sfAction (SyncFile p a)) ++ ") - " ++ (sfPath (SyncFile p a))
+  create (SyncFile p a) = "(" ++ show (sfAction (SyncFile p a)) ++ ") - " ++ (sfPath (SyncFile p a))
+  modif (SyncFile p a) = "(" ++ show (sfAction (SyncFile p a)) ++ ") - " ++ (sfPath (SyncFile p a))
 
 watchMe e = case e of
-  (Created isd f) -> print $ create (SyncFile f)
-  (MovedIn isd f mc) -> print $ "i detect move in : " ++ f
-  (MovedOut isd f mc) -> print $ "i detect move out : " ++ f
-  (Deleted isd f) -> print $ "i detect delete action : " ++ f
-  (Modified isd f) -> case f of
-                           (Just f') -> print $ "i detect modif action : " ++ f'
+  (IN.Created isd f) -> print $ create (SyncFile f Create)
+  (IN.MovedIn isd f mc) -> print $ move (SyncFile f Move)
+  (IN.MovedOut isd f mc) -> print $ move (SyncFile f Move)
+  (IN.Deleted isd f) -> print $ delete (SyncFile f Delete)
+  (IN.Modified isd f) -> case f of
+                           (Just f') -> print $ modif (SyncFile f' Update)
                            Nothing -> print "err modif action"
   otherwise -> print "other event ..."
   
 main = do
-  inotify <- initINotify
-  wd <- addWatch inotify [Create, Move, Delete, Modify] watchPath watchMe
-  putStrLn "-- watch " ++ watchPath
+  inotify <- IN.initINotify
+  wd <- IN.addWatch inotify [IN.Create, IN.Move, IN.Delete, IN.Modify] watchPath watchMe
+  putStrLn $ "-- watch " ++ watchPath
   getLine
-  removeWatch wd
-  putStrLn "Bye ..."
+  IN.removeWatch wd
+  putStrLn $ "-- Bye ..." ++ watchPath
   where
     watchPath = "/tmp/sb"
